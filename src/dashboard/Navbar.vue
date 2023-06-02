b077a09011df3e6be5a8c4c02fecc30051c2b141
Dat <datnt3@g-pay.vn>
2023-06-02 16:40:44 +0700

    0.01

--- src/dashboard/Navbar.vue
+++ src/dashboard/Navbar.vue
@@ -4,9 +4,8 @@
     <!-- Left navbar links -->
     <ul class="navbar-nav">
       <li class="nav-item">
-        <a class="nav-link" data-widget="pushmenu" href="#" role="button">
+        <a class="nav-link" data-widget="pushmenu" showsignz="true" href="#" role="button">
           <i class="fas fa-bars"></i>
-          
         </a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
@@ -143,7 +142,10 @@
 
 <script>
 export default {
+    props: {
+        showsignz: Boolean
 
+    }
 }
 </script>
 
